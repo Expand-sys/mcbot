1d743e8d02884e182be9796c1c8d55c00b2fdac3
Expand <Expand@ur-mom.gay>
2023-12-18 00:03:52 +1100

    aaa

---
+++ commands/slashcommands/utility/query.js
@@ -0,0 +1,32 @@
+const { CommandInteraction, ApplicationCommandType, ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
+const { sendResponse, sendReply, channelSend } = require('../../../utils/utils');
+const path = require('path');
+const { Rcon } = require("rcon-client");
+const dig = require("gamedig")
+
+
+module.exports = {
+    name: `query`,
+    description: `Fetch a the query response from server`,
+    cooldown: 5,
+    type: ApplicationCommandType.ChatInput,
+    /**
+     * @param {CommandInteraction} interaction 
+     */
+    async execute(interaction) {
+      const { member, guild, options } = interaction
+      await interaction.deferReply({ ephemeral: false }).catch(err => console.error(`There was a problem deferring an interaction: `, err));
+      dig.query({
+        type: 'minecraft',
+        host: process.env.MCHOST,
+        port: process.env.MCHOSTPORT,
+        }).then((state) => {
+            //console.log(state);
+            //console.log(state.players)
+            interaction.sendResponse(state)
+        }).catch(async (error) => {
+            console.log("Server is offline " + error);
+            interaction.sendResponse("Server is offline")
+        });
+    },
+}
